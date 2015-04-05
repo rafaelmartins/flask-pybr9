891f5b75196112dba62bac3a2e9980112572a76f
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2015-04-05 03:46:16 -0300

    added some magic to show json properly

--- mirror.sh
+++ mirror.sh
@@ -9,6 +9,15 @@ wget \
     --content-on-error \
     http://127.0.0.1:5000/
 
+sed \
+    -i \
+    "s,examples/ex11,examples/ex11.txt,g" \
+    "127.0.0.1:5000/index.html"
+
+mv \
+    "127.0.0.1:5000/examples/ex11" \
+    "127.0.0.1:5000/examples/ex11.txt"
+
 github-pages-publish \
     --verbose \
     --cname pybr9.rafaelmartins.eng.br \
